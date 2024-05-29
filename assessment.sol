8c00447fb1fc8ec128b328d7d8f27167b0248271
JoshuKierlAtok <137780306+JoshuKierlAtok@users.noreply.github.com>
2024-05-30 01:15:00 +0800

    Create module 2 assessment.sol

---
+++ assessment.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Assessment {
+    address payable public owner;
+    uint256 public balance;
+
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+    }
+
+    function getBalance() public view returns(uint256){
+        return balance;
+    }
+
+    function deposit() public payable {
+        require(msg.sender == owner, "You are not the owner of this account");
+        balance += msg.value;
+
+        emit Deposit(msg.value);
+    }
+
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+        
+        balance -= _withdrawAmount;
+        (bool success, ) = owner.call{value: _withdrawAmount}("");
+        require(success, "Transfer failed.");
+
+        emit Withdraw(_withdrawAmount);
+    }
+}
